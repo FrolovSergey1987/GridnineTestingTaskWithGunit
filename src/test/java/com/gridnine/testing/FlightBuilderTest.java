febc844fa34cdb9c133f1662519504e868524028
FrolovSergey1987 <85957482+FrolovSergey1987@users.noreply.github.com>
2021-12-21 11:45:44 +0300

    21.12.2021 - add FlightBuilderTest isEmpty

---
+++ src/test/java/com/gridnine/testing/FlightBuilderTest.java
@@ -0,0 +1,14 @@
+package com.gridnine.testing;
+
+import junit.framework.TestCase;
+
+public class FlightBuilderTest extends TestCase {
+
+    public void testCreateFlights() throws Exception {
+        if (FlightBuilder.createFlights().isEmpty()){
+            System.out.println("No flights!!!");
+        }
+        System.out.println(FlightBuilder.createFlights());
+
+    }
+}
