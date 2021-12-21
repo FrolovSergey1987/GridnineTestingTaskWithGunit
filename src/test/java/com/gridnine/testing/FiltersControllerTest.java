9bc2892241e3e06ded2eb4322b282a9ee20c6c6f
FrolovSergey1987 <85957482+FrolovSergey1987@users.noreply.github.com>
2021-12-21 11:50:34 +0300

    21.12.2021 - add FlightBuilderTest + FiltersControllerTest

--- src/test/java/com/gridnine/testing/FiltersControllerTest.java
+++ src/test/java/com/gridnine/testing/FiltersControllerTest.java
@@ -6,11 +6,20 @@ public class FiltersControllerTest extends TestCase {
 
 
     public void testDepartureDateBeforeCurrentDate() throws Exception {
+        if (FlightBuilder.createFlights().isEmpty()){
+            System.out.println("No flights!!!");
+        }
     }
 
     public void testSegmentsDateArrivalBeforeDateDeparture() throws Exception {
+        if (FlightBuilder.createFlights().isEmpty()){
+            System.out.println("No flights!!!");
+        }
     }
 
     public void testTransferOverTwoHour() throws Exception {
+        if (FlightBuilder.createFlights().isEmpty()){
+            System.out.println("No flights!!!");
+        }
     }
 }
